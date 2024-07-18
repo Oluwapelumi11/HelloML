18277544c161760b0820684566b7c4dca2907a1d
Oluwapelumi Sunday <oluwapelumiezekiel11@gmail.com>
2024-07-18 10:36:42 +0100

    Add project files.

---
+++ HelloML.Trainer/ModelBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloML.Model;
+
+namespace HelloML.Trainer
+{
+    public static class ModelBuilder
+    {
+        private static MLContext _context  = new MLContext();
+
+        //MAin method
+        public static void CreateModel(string inputDataFileName,string outputDataFileName)
+        {
+            // Load the data
+            var dataview = _context.Data.LoadFromTextFile<ModelInput>(inputDataFileName);
+            // Build the training pipeline
+            var dataProcessPipeline = _context.Transforms.Text.FeaturizeText("");
+            var trainer = _context.Regression.Trainers.OnlineGradientDescent("");
+            // Train the Model
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+            // Quick Evaluation of the Model
+
+
+            // Save the output Model
+        }
+    }
+}
